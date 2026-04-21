7e39ef2e37632a742c92dc2de3ea616856543e40
douglasreveco-ux <douglasreveco@gmail.com>
2026-04-21 13:31:33 -0400

    Inicialización proyecto HTTPS

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import https from "https";
+import fs from "fs";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor HTTPS funcionando 🔐");
+});
+
+const options = {
+  pfx: fs.readFileSync("cert.pfx"),
+  passphrase: "1234"
+};
+
+https.createServer(options, app).listen(8080, () => {
+  console.log("Servidor en https://localhost:8080");
+});
